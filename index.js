565fc49789aa7f06638d2a187314d17ba701b23a
Chico <emanuelsousa0140@gmail.com>
2024-02-12 22:55:50 -0300

    removendo código desnecessário

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var elementosDuvida = document.querySelectorAll('.duvida')
 
-/*elementosDuvida.forEach(function (duvida) {
-    duvida.addEventListener('click', function () {
-        duvida.classList.toggle('ativa')
-    })
-});*/
-
 elementosDuvida.forEach((duvida) => {
     duvida.addEventListener('click', function () {
         duvida.classList.toggle('ativa')
